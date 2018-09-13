b0fa8697180d5a0d43d6967686ac3d6c92ae8095
Lin <epoch0ash@gmail.com>
2018-09-14 01:44:53 +0800

    dev update

--- src/com/ncgroup/bp/Startup.java
+++ src/com/ncgroup/bp/Startup.java
@@ -22,8 +22,32 @@ public class Startup {
 
 			// 初始化讯飞语音合成SDK
 			SpeechUtility.createUtility(SpeechConstant.APPID + "=" + MY_APPID);
-			Speech s = new Speech();
-			s.play("定时任务管理器实例");	
+
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						// 定时任务管理器实例
+						TimerManager tm = TimerManager.getInstance();
+						int weekDay = -1;
+						while (true) {
+							weekDay = DateHelper.getWeekDayByCurrentTime();
+							// 周末停止任务
+							if (weekDay == 6 || weekDay == 1) {
+								if (!tm.isTimerTaskStopped())
+									tm.stopTimerTask();
+								// 工作日开始任务
+							} else {
+								if (tm.isTimerTaskStopped())
+									tm.startTimerTask();
+							}
+						}
+					} catch (Exception e) {
+						Log.error("工作线程异常！", e);
+					}
+				}
+			}).start();
+
 		} catch (Exception e) {
 			Log.error("主程序异常！", e);
 		}
